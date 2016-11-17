147f3f5ec580b04a41a016ecbdef6058cec852bd
ajaysundark <ajaysundar.k@gmail.com>
2016-11-17 02:27:18 -0600

    nav bar styling

--- client/src/NavigationBar.js
+++ client/src/NavigationBar.js
@@ -15,12 +15,16 @@ var NavigationBar = React.createClass({
   render(){
     console.log('Starting render navbar');
     console.log('With home ',this.checkactive("Home"));
+    var username = "My Profile";
+    if (this.props.username) {username = this.props.username;}
     return (
       <ul>
-        <li><a className={this.checkactive("Home")} href={this.props.myfeedlink}>Home</a></li>
-        <li><a className={this.checkactive("Explore")} href={this.props.explorelink}>Explore</a></li>
-        <li><a className={this.checkactive("MyPost")} href={this.props.mypostslink}>My Posts</a></li>
-        <li className= "showright"><a className="active" href={this.props.addcontentlink}>+</a></li>
+        <li><a className={"crows-brand "+this.checkactive("Explore")} href={this.props.explorelink}>W E B C R O W S</a></li>
+        <li><a className={this.checkactive("Home")} href={this.props.myfeedlink}>Feed</a></li>
+        <li className= "showright"><a className="active" href={this.props.addcontentlink}>
+          <i className="fa fa-plus crows-add-fa"></i></a>
+        </li>
+        <li className="showright user-li"><a className={this.checkactive("MyPost")} href={this.props.mypostslink}>{username}</a></li>
       </ul>
     )
   }
